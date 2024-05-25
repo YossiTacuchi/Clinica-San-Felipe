030abde0b190690e498318988a73b0a1a59f238d
Yoshi Tacuchi <yossitacuchi@gmail.com>
2024-05-25 00:01:20 -0500

    owl

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,22 @@ $(document).ready(function() {
     }
     lastScrollTop = scrollTop;
   });
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
 });
