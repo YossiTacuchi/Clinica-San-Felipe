5d4eb18e196d36a6c057bd0fd7dc82b6790ba750
Yoshi Tacuchi <yossitacuchi@gmail.com>
2024-05-27 21:53:46 -0500

    fixes home

--- js/scripts.js
+++ js/scripts.js
@@ -28,6 +28,26 @@ $(document).ready(function() {
       return false;
     });
 
+
+    $('.owl-carousel-home').owlCarousel({
+      autoplay:true,
+      loop:true,
+      margin:10,
+      nav:true,
+      dots:false,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+    });
+
   $('.owl-carousel-articulos').owlCarousel({
     loop:true,
     margin:10,
