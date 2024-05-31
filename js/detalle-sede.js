9722aaa0803bc4d7b8e14a91ce20c144c92e2577
Yoshi Tacuchi <yossitacuchi@gmail.com>
2024-05-31 00:10:55 -0500

    split js

---
+++ js/detalle-sede.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+    $('.owl-carousel-detalle-sede').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
+    $('.owl-carousel-especialidades').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+      });
+
+});
---
+++ js/home.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+    $('.owl-carousel-home').owlCarousel({
+        autoplay:true,
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+      });
+  
+    $('.owl-carousel-articulos').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      dots:false,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:4
+          }
+      }
+    });
+
+    $('.owl-carousel-sedes').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+      });
+
+});
