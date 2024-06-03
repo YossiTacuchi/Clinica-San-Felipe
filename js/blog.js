fdb70ad46c272b3b57e0a85c8b19cc24657eae7f
Yoshi Tacuchi <yossitacuchi@gmail.com>
2024-06-03 13:24:47 -0500

    blog

--- js/blog.js
+++ js/blog.js
@@ -8,4 +8,30 @@ $(document).ready(function() {
 
             $('.list-tags .tag').addClass('ocultar');
         }else{
-            $(this).find('.m
+            $(this).find('.mas').addClass('ocultar');
+            $(this).find('.menos').removeClass('ocultar');
+
+            $('.list-tags .tag').removeClass('ocultar');
+        }
+        
+    });
+
+    $('.owl-carousel-ultimas').owlCarousel({
+        loop:true,
+        margin:20,
+        nav:false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:2
+            }
+        }
+    });
+
+});
