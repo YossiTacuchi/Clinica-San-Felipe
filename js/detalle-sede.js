33af8efd238f00c816b223e1ad8d56135f91f2ac
Yoshi Tacuchi <yossitacuchi@gmail.com>
2024-06-05 23:05:29 -0500

    la molina

--- js/detalle-sede.js
+++ js/detalle-sede.js
@@ -55,6 +55,25 @@ $(document).ready(function() {
                 items:4
             }
         }
-      });
+      });   
+
+
+      function updateImages() {
+        var width = $(window).width();
+        var $img = $('.mapa').find('.img-mapa');
+        var mobileSrc = $img.data('src-mobile');
+        var desktopSrc = $img.attr('src');
+
+        if (width < 600) {
+        if ($img.attr('src') !== mobileSrc) {
+            $img.attr('src', mobileSrc);
+        }
+        } else {
+        if ($img.attr('src') !== desktopSrc) {
+            $img.attr('src', desktopSrc);
+        }
+        }
+      }
+      $(window).resize(updateImages);
 
 });
