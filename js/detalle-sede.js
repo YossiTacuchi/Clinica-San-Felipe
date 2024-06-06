c1da7d0bcac6c857e990e705b232bf0df2decf3b
Yoshi Tacuchi <yossitacuchi@gmail.com>
2024-06-05 22:13:06 -0500

    home

--- js/detalle-sede.js
+++ js/detalle-sede.js
@@ -26,4 +26,35 @@ $(document).ready(function() {
         margin:10,
         nav:false,
         dots:true,
-        responsi
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
+    $('.owl-carousel-especialidades').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+      });
+
+});
